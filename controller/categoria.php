b492cc9a3761ec035a8e456a44cd4eae7727d569
ElLuchoMan <luchoman96@gmail.com>
2021-02-12 11:35:48 -0500

    Agregado el método POST

--- controller/categoria.php
+++ controller/categoria.php
@@ -1,12 +1,18 @@
 <?php
+header('Content-Type: application/json');
 require_once("../config/conexion.php");
 require_once("../models/Categoria.php");
 $categoria=new Categoria();
+$body=json_decode(file_get_contents("php://input"),true);
 switch($_GET["op"]){
     case "GetAll":
 $datos=$categoria->get_categoria();
 echo json_encode($datos);        
 break;
+case "GetID":
+    $datos=$categoria->get_categoria_por_id($body["cat_id"]);
+    echo json_encode($datos);
+    break;
 }
 
 ?>
--- models/Categoria.php
+++ models/Categoria.php
@@ -6,8 +6,18 @@ public function get_categoria(){
     $sql="SELECT * FROM categoria WHERE est=1";
     $sql=$conectar->prepare($sql);
     $sql->execute();
-    return $resultado=$sql->fetchAll();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
 }
+public function get_categoria_por_id($cat_id){
+    $conectar=parent::conexion();
+    parent::set_names();
+    $sql="SELECT * FROM categoria WHERE est = 1 AND cat_id = ?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1,$cat_id);
+    $sql->execute();
+    return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
 }
 
 ?>
